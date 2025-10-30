ba15920ad3b633db9718c81f4ac382bf4764a30c
hansnone <decabopedro@gmail.com>
2025-10-30 16:53:08 +0100

    commit

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿static double SolicitarNumero()
+{
+    Console.WriteLine("Dame un numero.");
+    Console.Write(">> ");
+    string numero = Console.ReadLine();
+    if (double.TryParse(numero, out double numero2))
+    {
+        return numero2;
+    }
+    else
+    {
+        Console.WriteLine("Entrada invalida. Intente de nuevo.");
+        return SolicitarNumero();
+    }
+}
+static string SolicitarOperacion()
+{
+    Console.WriteLine("Operaciones validas: +, -, * y //.");
+    string entrada = Console.ReadLine();
+    inicio:
+    switch (entrada)
+    {
+        case "+":
+            Console.WriteLine("Suma.");
+            return entrada;
+            break;
+        case "-":
+            Console.WriteLine("Resta.");
+            return entrada;
+            break;
+        case "*":
+            Console.WriteLine("Multiplicacion.");
+            return entrada;
+            break;
+        case "/":
+            Console.WriteLine("Division.");
+            return entrada;
+            break;
+        default:
+            Console.WriteLine("Operacion no permitida.");
+            return SolicitarOperacion(); 
+            break;
+    }
+}
+
+double a = SolicitarNumero();
+double b = SolicitarNumero();
+string op = SolicitarOperacion();
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(op);
+
+static string Calcular(double a, double b, string op)
+{
+    if (op == "*")
+    {
+        Console.WriteLine(a * b);
+    }
+}
